a59b72e36c5b3cddbbc17bda99fd28e489475b97
Diyar Karlybai <d_karlybai@kbtu.kz>
2026-02-07 17:50:32 +0500

    p2

---
+++ pr2/boolean/boolean_operators.py
@@ -0,0 +1,5 @@
+x = 5
+y = 10
+
+print(x > 0 and y > 0) 
+print(x > 10 and y > 5) 
---
+++ pr2/if_else/if_else.py
@@ -0,0 +1,5 @@
+number=int(input())
+if number % 2 == 0:
+    print(f"{number} is even")
+else:
+    print(f"{number} is odd")
